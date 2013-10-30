cef39efa939da7beb1018eb729158b6c23614e8b
Daniel Cestari <dcestari@gmail.com>
2013-10-30 18:15:16 -0430

    Add contact information to the User model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 # Users
-user_john = User.create email: 'john.doe@example.com', password: '12345678'
-user_jane = User.create email: 'jane.doe@example.com', password: '12345678'
+user_john = User.create email: 'john.doe@example.com', password: '12345678', first_name: 'John', last_name: 'Doe', phone_number: '555-1037'
+user_jane = User.create email: 'jane.doe@example.com', password: '12345678', first_name: 'Jane', last_name: 'Doe', phone_number: '555-1037'
 
-user_peter = User.create email: 'peter.parker@example.com', password: '12345678'
-user_bruce = User.create email: 'bruce.wayne@example.com',  password: '12345678'
+user_peter = User.create email: 'peter.parker@example.com', password: '12345678', first_name: 'Peter', last_name: 'Parker', phone_number: '555-1037'
+user_bruce = User.create email: 'bruce.wayne@example.com',  password: '12345678', first_name: 'Bruce', last_name: 'Wayne', phone_number: '555-1037'
 
 # Pets
 dog_pancho = user_john.pets.create name: 'Pancho', kind: Pet::DOG, photo: File.open(File.join(Rails.root, 'public/samples/dog1.jpg'))

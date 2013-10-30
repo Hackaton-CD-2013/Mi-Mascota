75c47be6505b6d4932f8a04c4f08e3fb1f1d236c
Daniel Cestari <dcestari@gmail.com>
2013-10-30 09:31:48 -0430

    Initial Commit: Rails 4.0 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackaton::Application.config.secret_key_base = '2b993a1f1fed6b72f73ed686671c5234aa8cdddf789e299f031fe7203eb16c5e9307fb78b79b386bfbbcdd865ad70672ad4c458bb7cafca09d88aab78ee7838f'

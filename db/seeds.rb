af92287970b63d6ddcee49b472a072539f4c467f
Daniel Cestari <dcestari@gmail.com>
2013-10-30 20:35:21 -0430

    Add location information for services on seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,16 +13,33 @@ cat_agatha = user_jane.pets.create name: 'Agatha', kind: Pet::CAT, photo: File.o
 cat_minina = user_jane.pets.create name: 'Minina', kind: Pet::CAT, photo: File.open(File.join(Rails.root, 'public/samples/cat2.jpg'))
 
 services_by_category = {
-  'Veterinario' => [ 'Animal City', '+cotas Centro Veterinario' ],
-  'Tienda'      => [ 'Don Perro', 'El Rincon de la Mascota', '+kotas los Naranjos', 'Mascotienda' ],
-  'Pensión'     => [ 'Pensión Canina La Julia', 'Pets & Care' ],
+  'Veterinario' => [
+    { name: 'Animal City', address: 'Av. Humboldt, Edif. Las Mercedes, Local 1, San Bernardino, Caracas', latitude: 10.520303, longitude: -66.904142 },
+    { name: '+cotas Centro Veterinario', address: 'Av. Miguel Angel con calle Don Bosco, nivel PB. Colinas de Bello Monte', latitude: 10.480132, longitude: -66.878242 },
+  ],
+  'Tienda' => [
+    { name: 'Don Perro', description: 'Venta de Equipos, Artículos y Accesorios para Animales', address: 'Av. Santa Teresa de Jesús, Qta. Don Perro, Diagonal al C. C. San Ignacio, La Castellana, Caracas', latitude: 10.498256, longitude: -66.855127 },
+    { name: 'El Rincon de la Mascota', address: 'C. C. Plaza Las Américas 1, PB, Local Nº. 2, El Cafetal, Caracas', latitude: 10.458525, longitude: -66.828954 },
+    { name: '+kotas los Naranjos', address: 'C. C. Los Naranjos, Local Nº. 19, Los Naranjos-cafetal, Caracas', latitude: 10.444788, longitude: -66.83404 },
+    { name: 'Mascotienda', address: 'Av. Cataluña, entre América y El Parque, Edif. Terepaima, Piso PB, Local Nº. 2, Las Acacias, Caracas', latitude: 10.483592, longitude: -66.887083 },
+  ],
+  'Pensión' => [
+    { name: 'Pensión Canina La Julia', address: 'La Julia, Caracas', latitude: 10.507391, longitude: -66.811638 },
+    { name: 'Pets & Care', address: 'A domicilio' }
+  ],
 }
 
 services_by_category.each do |name, services|
   category = Category.create name: name
 
   services.each do |service|
-    category.services.create name: service
+    if service.class == Hash
+      service_params = service
+    else
+      service_params = { name: service }
+    end
+
+    category.services.create service_params
   end
 end
 
